7e7be12e1d17247893aab9affc58efb2b91706a6
PabloFC <pablofc_68@hotmail.com>
2025-01-16 20:19:01 +0100

    creating main box

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-bold text-blue-500">
-        Hola mundo con tailwind
-      </h1>
+      <div className="h-screen flex items-center justify-center">
+        <div className="app max-w-[600px] w-full">
+          <div className="bg-red-900 flex justify-between items-center p-4">
+            <div>
+              <h1>cal</h1>
+            </div>
+            <div>
+              <p>Theme</p>
+              <input type="range" name="" id="" />
+            </div>
+          </div>
+          <div>
+            <input type="text" className="w-full h-20 text-right" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
