2a56a27fc6e0eb760a6f8f2f2860923c91584f2f
PabloFC <pablofc_68@hotmail.com>
2025-01-19 12:17:27 +0100

    adding cllassNames to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="h-screen flex items-center justify-center">
-        <section className="app max-w-[600px] w-full">
+        <section className="app max-w-[600px] w-full bg-orange-400">
           <div className="bg-red-900 flex justify-between items-center p-4">
             <div>
               <h1>cal</h1>
@@ -17,25 +17,29 @@ function App() {
           <section>
             <input type="text" className="w-full h-20 text-right" />
           </section>
-          <section className="grid grid-cols-4 gap-2">
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>DEL</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>-</button>
-            <button>.</button>
-            <button>0</button>
-            <button>/</button>
-            <button>*</button>
-            <button>Reset</button>
-            <button>=</button>
+          <section className="bg-blue-900 p-4 grid grid-cols-4 gap-2">
+            <button className="number">7</button>
+            <button className="number">8</button>
+            <button className="number">9</button>
+            <button className="operator">DEL</button>
+
+            <button className="number">4</button>
+            <button className="number">5</button>
+            <button className="number">6</button>
+            <button className="operator">+</button>
+
+            <button className="number">1</button>
+            <button className="number">2</button>
+            <button className="number">3</button>
+            <button className="operator">-</button>
+
+            <button className="number">.</button>
+            <button className="number">0</button>
+            <button className="number">/</button>
+            <button className="operator">*</button>
+
+            <button className="reset">Reset</button>
+            <button className="equal">=</button>
           </section>
         </section>
       </div>
