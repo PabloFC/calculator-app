250e01f87f6a4f8ec0e7890b576fe5c81f8e647d
PabloFC <pablofc_68@hotmail.com>
2025-01-19 11:40:45 +0100

    adding buttons sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="h-screen flex items-center justify-center">
-        <div className="app max-w-[600px] w-full">
+        <section className="app max-w-[600px] w-full">
           <div className="bg-red-900 flex justify-between items-center p-4">
             <div>
               <h1>cal</h1>
@@ -14,10 +14,30 @@ function App() {
               <input type="range" name="" id="" />
             </div>
           </div>
-          <div>
+          <section>
             <input type="text" className="w-full h-20 text-right" />
-          </div>
-        </div>
+          </section>
+          <section className="grid grid-cols-4 gap-2">
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>DEL</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>-</button>
+            <button>.</button>
+            <button>0</button>
+            <button>/</button>
+            <button>*</button>
+            <button>Reset</button>
+            <button>=</button>
+          </section>
+        </section>
       </div>
     </>
   );
