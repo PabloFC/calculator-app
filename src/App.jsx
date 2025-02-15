0fbb4bc6ef522558929c3e8ba3626ea58235736e
PabloFC <pablofc_68@hotmail.com>
2025-02-15 23:32:48 +0100

    useCalculator custom hook added

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,16 @@
-import { useState } from "react";
 import CalculatorButtons from "./components/CalculatorButtons";
 import { useTheme } from "./hooks/useTheme";
 import "./App.css";
+import { useCalculator } from "./hooks/useCalculator";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
+  const { inputValue, handleButtonsClick } = useCalculator(15);
   const { translateX, handleThemeChange } = useTheme();
-
-  const maxInputNumbers = 15;
-
-  const handleButtonsClick = (value) => {
-    if (value === "=") {
-      try {
-        if (inputValue.includes("/0")) {
-          setInputValue("Can't divide by 0");
-          return;
-        }
-        const calculatedResult = eval(inputValue);
-        setInputValue(calculatedResult.toString());
-      } catch {
-        setInputValue("Error");
-      }
-    } else if (value === "DEL") {
-      setInputValue((prevValue) => prevValue.slice(0, -1));
-    } else if (value === "RESET") {
-      setInputValue("");
-    } else {
-      setInputValue((prevValue) => {
-        const numberCount = prevValue.replace(/[^0-9]/g, "").length;
-
-        if (value.match(/[0-9]/) && numberCount >= maxInputNumbers) {
-          return prevValue;
-        }
-        return prevValue + value;
-      });
-    }
-  };
 
   return (
     <main className="mt-6 mb-6 p-6">
       <div className="h-screen flex items-center justify-center ">
-        <section className="calculator max-w-[600px] w-full">
+        <section className="max-w-[600px] w-full">
           <div className="flex justify-between items-center p-4">
             <div>
               <span className="logoStyle ">calc</span>
---
+++ src/hooks/useCalculator.js
@@ -0,0 +1,36 @@
+// useCalculator.js
+import { useState } from "react";
+
+export const useCalculator = (maxInputNumbers) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleButtonsClick = (value) => {
+    if (value === "=") {
+      try {
+        if (inputValue.includes("/0")) {
+          setInputValue("Can't divide by 0");
+          return;
+        }
+        const calculatedResult = eval(inputValue);
+        setInputValue(calculatedResult.toString());
+      } catch {
+        setInputValue("Error");
+      }
+    } else if (value === "DEL") {
+      setInputValue((prevValue) => prevValue.slice(0, -1));
+    } else if (value === "RESET") {
+      setInputValue("");
+    } else {
+      setInputValue((prevValue) => {
+        const numberCount = prevValue.replace(/[^0-9]/g, "").length;
+
+        if (value.match(/[0-9]/) && numberCount >= maxInputNumbers) {
+          return prevValue;
+        }
+        return prevValue + value;
+      });
+    }
+  };
+
+  return { inputValue, handleButtonsClick };
+};
